b2ab72275a7a17beb95b6a3134cb07fb9366ed21
Sharath S <sharathsv06@gmail.com>
2026-09-06 06:08:05 +0000

    bott.py

--- bott.py
+++ bott.py
@@ -759,7 +759,8 @@ async def lyrics(ctx, *, song: str = None):
 # RUN BOT
 # =========================
 
-with open("Token.txt", "r", encoding="utf-8") as f:
-    TOKEN = f.read().strip()
+import os
 
-bot.run(TOKEN)
+token = os.getenv("TOKEN")
+
+bot.run(token)
